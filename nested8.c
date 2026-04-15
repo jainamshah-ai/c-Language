cc3a701f6643c9a65c3b0ef2af33c9e4d7de2ed1
meetpatel0264 <meetpatel0264@gmail.com>
2026-04-15 17:26:54 +0530

    15april

---
+++ nested8.c
@@ -0,0 +1,658 @@
+#include<stdio.h>
+
+void main(){
+	
+	int a,b,c,d,e,f,g,h;
+	printf("enter the value");
+	scanf("%d",&a);
+	printf("enter the value");
+	scanf("%d",&b);
+	printf("enter the value");
+	scanf("%d",&c);
+	printf("enter the value");
+	scanf("%d",&d);
+	printf("enter the value");
+	scanf("%d",&e);
+	printf("enter the value");
+	scanf("%d",&f);
+	printf("enter the value");
+	scanf("%d",&g);
+	printf("enter the value");
+	scanf("%d",&h);
+	
+	if(a>=b){
+//		a is max
+		if(a>=c){
+//			a is max
+			if(a>=d){
+//				a is max
+				if(a>=e){
+//					a is max
+					if(a>=f){
+//						a is max
+						if(a>=g){
+//							a is max
+							if(a>=h){
+								printf("the maximum value is %d",a);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}else{
+//					e is max
+					if(e>=f){
+//						e is max
+						if(e>=g){
+//							f is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is masx
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+				
+			}else{
+//				 d is max
+				if(d>=e){
+//					d is max
+					if(d>=f){
+//						d is max
+						if(d>=g){
+//							d is max
+							if(d>=h){
+								printf("the maximum value is %d",d);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							f is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}else{
+//					e is max
+					if(e>=f){
+//						f is max
+						if(e>=g){
+//							g is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+			}
+
+		}else{
+//			c is max
+			if(c>=d){
+//				c is max
+				if(c>=e){
+//					c is max
+					if(c>=f){
+//						c is max
+						if(c>=g){
+//							c is max
+							if(c>=h){
+								printf("the maximum value is %d",c);
+							}else{
+							}
+							printf("the maximum value is %d",h);
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}else{
+//					e is max
+					if(e>=f){
+//						e is max
+						if(e>=g){
+//							e is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+			}else{
+//				d is max
+				if(d>=e){
+//					d is max
+					if(d>=f){
+//						d is max
+						if(d>=g){
+//							d is max
+							if(d>=h){
+								printf("the maximum value is %d",d);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+						}
+					}
+				else{
+					if(e>=f){
+//						e is max
+						if(e>=g){
+//							e is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+			}
+		}
+	}else{
+//		b is max
+			if(b>=c){
+//			b is max
+			if(b>=d){
+//				b is max
+				if(b>=e){
+//					b is max
+					if(b>=f){
+//						b is max
+						if(b>=g){
+//							b is max
+							if(b>=h){
+								printf("the maximum value is %d",b);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}else{
+//					e is max
+					if(e>=f){
+//						e is max
+						if(e>=g){
+//							f is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is masx
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+				
+			}else{
+//				 d is max
+				if(d>=e){
+//					d is max
+					if(d>=f){
+//						d is max
+						if(d>=g){
+//							d is max
+							if(d>=h){
+								printf("the maximum value is %d",d);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							f is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}else{
+//					e is max
+					if(e>=f){
+//						f is max
+						if(e>=g){
+//							g is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+			}
+
+		}else{
+//			c is max
+			if(c>=d){
+//				c is max
+				if(c>=e){
+//					c is max
+					if(c>=f){
+//						c is max
+						if(c>=g){
+//							c is max
+							if(c>=h){
+								printf("the maximum value is %d",c);
+							}else{
+							}
+							printf("the maximum value is %d",h);
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}else{
+//					e is max
+					if(e>=f){
+//						e is max
+						if(e>=g){
+//							e is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+			}else{
+//				d is max
+				if(d>=e){
+//					d is max
+					if(d>=f){
+//						d is max
+						if(d>=g){
+//							d is max
+							if(d>=h){
+								printf("the maximum value is %d",d);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+						}
+					}
+				else{
+					if(e>=f){
+//						e is max
+						if(e>=g){
+//							e is max
+							if(e>=h){
+								printf("the maximum value is %d",e);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}else{
+//						f is max
+						if(f>=g){
+//							f is max
+							if(f>=h){
+								printf("the maximum value is %d",f);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}else{
+//							g is max
+							if(g>=h){
+								printf("the maximum value is %d",g);
+							}else{
+								printf("the maximum value is %d",h);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ while-loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+	
+	int a=1;
+	
+	while(a<=10)
+	{
+		
+		printf("%d-%d\n",a,a*a);
+		a++;
+	}
+}
